bcdd6d12502b5eac6b5af21cabc1b53d2a0ba7f0
guangzhen <guangzhen.li@intel.com>
2015-01-08 15:41:28 +0800

    Add mapping from cocos2dx to binding

--- targets/idl/templates/layout_foot.c
+++ targets/idl/templates/layout_foot.c
@@ -6,16 +6,18 @@
         m_cocos2d_impl = impl;
     }
 
+#set generator = $current_class.generator
     $current_class.target_class_name::${current_class.target_class_name}() {
         #if not $current_class.is_abstract
         m_cocos2d_impl = new cocos2d::${current_class.target_class_name}();
+        #if $generator.in_listed_extend_classed($current_class.class_name) and not $current_class.is_abstract
+        m_cocos2d_impl->setCrosswalkImpl((void*)this);
+        #end if
         #end if
     }
 
-#set generator = $current_class.generator
 #if $generator.in_listed_extend_classed($current_class.class_name) and not $current_class.is_abstract
     void V8$current_class.target_class_name::ctorMethodCustom(const v8::FunctionCallbackInfo<v8::Value>& info) {
-        //TODO: add proxy
     }
 #end if
     ##void $current_class.target_class_name::setCocos2dImpl(cocos2d::$current_class.target_class_name impl) {
